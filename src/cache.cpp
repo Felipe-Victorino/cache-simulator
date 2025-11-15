ad1376a2c882acf528ae7a9d3dc27deefb10495b
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-11-15 16:01:19 -0300

    feat(cache): Memória é um objeto separado, a linha da cache é um struct, não tem mais constantes de preprocessador

--- src/cache.cpp
+++ src/cache.cpp
@@ -1,27 +1,32 @@
 #include "cache.hpp"
 
-Cache::Cache(std::string name, int latency, int associativity, int set_size)
+Cache::Cache(std::string name, int latency, int associativity, int set_amount, WritePolicy policy)
 {
     this->p_name = name;
     this->p_latency = latency;
     this->p_associativity = associativity;
-    this->p_total_sets = set_size;
-    this->p_policy = WRITE_BACK;
+    this->p_set_amount = set_amount;
+    this->p_policy = policy;
 };
 
-int Cache::calculateCacheSize(){
-    int size = this->p_linesize * this->p_total_sets * this->p_associativity;
+uint32_t Cache::calculateCacheSize(){
+    int size = this->p_linesize * this->p_set_amount * this->p_associativity;
     return size;
 };
 
+uint32_t Cache::calculateLineTotal(){
+    uint32_t total = this->p_associativity * this->p_set_amount;
+    return total;
+};
+
 void Cache::printCache(){
-    std::cout << this->p_name << std::endl;
-    std::cout << this->p_latency << std::endl;
-    std::cout << this->p_linesize << std::endl;
-    std::cout << this->p_associativity << std::endl;
-    std::cout << this->p_total_sets << std::endl;
-    std::cout << this->p_cache_size << std::endl;
-    std::cout << this->p_policy << std::endl;
+    std::cout << "Name: " << this->p_name << std::endl;
+    std::cout << "Latency: " << this->p_latency << std::endl;
+    std::cout << "Line size: " << this->p_linesize << std::endl;
+    std::cout << "Associativity: " << this->p_associativity << std::endl;
+    std::cout << "Sets: " << this->p_set_amount << std::endl;
+    std::cout << "Cache size: " << this->p_cache_size << std::endl;
+    std::cout << "Policy: " << this->p_policy << std::endl;
 
 }
 Cache::Cache()
--- src/cache.hpp
+++ src/cache.hpp
@@ -4,21 +4,21 @@
 #include <vector>
 #include <my-lib/bit.h>
 
-#define LINE_SIZE 32
-
-using BitSet = Mylib::BitSet<LINE_SIZE>;
+using BitSet = Mylib::BitSet<32>;
 /*
 A CACHE NÃO ARMAZENA DADOS REAIS, ELA É UM SIMULADOR DE TEMPO
 */
 
-
-
 //representa uma linha
-typedef struct {
-    bool dirty = false;
-    int32_t tag;
+// offset log2 tamanho linha
+// qtd total de linhas = associatividade * conjuntos
+
+struct CacheLine {
     int content : 1;
-} CacheLine;
+    uint32_t tag;
+    bool dirty = false;
+    u_int32_t age;
+};
 // guardar structs em um vetor até que se tenha o tamanho da cache;
 
 enum WritePolicy {WRITE_BACK, WRITE_THROUGH};
@@ -27,36 +27,39 @@ class MainMemory{
 private:
     std::string p_name;
     uint32_t p_latency;
-    uint32_t p_size ; // 32 bits, a quantidade de endereços da memória
+    uint32_t p_size = sizeof(uint32_t) * 8; // 32 bits, a quantidade de endereços da memória
 public:
 
     std::string getName(){return this->p_name;}
     uint32_t getLatency() {return this->p_latency;}
     void setName(std::string name){this->p_name = name;}
-    void setLatency(int latency){this->p_latency = latency;}
+    void setLatency(uint32_t latency){this->p_latency = latency;}
 };
 
-class Cache{
-
+class Cache
+{
 private:
     std::string p_name;
     uint32_t p_cache_size;
 
     uint32_t p_latency;
-    uint32_t p_linesize = LINE_SIZE;//tamanho de linha da cache
+    uint32_t p_linesize = 32; //tamanho de linha da cache
     uint32_t p_associativity; // quantas linhas de memórias são agrupadas em uma linha de cache
-    uint32_t p_total_sets;
-    std::vector<CacheLine> lines;
-    enum WritePolicy p_policy;
+    uint32_t p_set_amount;
+    std::vector<CacheLine> p_lines;
+    WritePolicy p_policy;
 
 public:
     Cache();
-    Cache(std::string name, int latency, int associativity, int set_size);
+    Cache(std::string name, int latency, int associativity, int set_amount, WritePolicy policy);
 
     uint32_t calculateCacheSize();
+    uint32_t calculateLineTotal();
     void printCache();
 
     // GETTERS E SETTERS
+
+    std::vector<CacheLine> getLines(){return this->p_lines;};
 
     std::string getName(){return this->p_name;}
     void setName(std::string name){this->p_name = name;}
@@ -72,8 +75,8 @@ public:
     uint32_t getAssociativity(){return this->p_associativity;}
     void setAssociativity(uint32_t associativity){this->p_associativity = associativity;}
 
-    uint32_t getAssociativeSet(){return this->p_total_sets;}
-    void setAssociativeSet(uint32_t sets){this->p_total_sets = sets;};
+    uint32_t getAssociativeSet(){return this->p_set_amount;}
+    void setAssociativeSet(uint32_t sets){this->p_set_amount = sets;};
 
     WritePolicy getPolicy(){return this->p_policy;};
     void setPolicy(WritePolicy policy){ this->p_policy = policy;}
